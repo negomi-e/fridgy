7dfdd61b68031506840315a61fc2ae8b6309a204
Aleksey-AV <aleksey.webmake@gmail.com>
2020-05-14 16:29:59 +0300

    bug

---
+++ frontend/src/components/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Nav from '../../Common/Nav/Navigation.jsx'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Homepage</h1>
+      </div>
+    )
+  }
+}
---
+++ frontend/src/redux/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  loginStatus: true
+}
+
+export function authReducer(state = initialState, action) {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+}
+
