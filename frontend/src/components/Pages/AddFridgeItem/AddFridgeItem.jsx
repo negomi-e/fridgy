cd57f893e3b696e3ffaeb3b2b97da0996bc6efd6
samolevich <samolevich@gmail.com>
2020-05-19 16:46:07 +0300

    tuesday

--- frontend/src/components/Pages/AddFridgeItem/AddFridgeItem.jsx
+++ frontend/src/components/Pages/AddFridgeItem/AddFridgeItem.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import SelectDate from '../SelectDate';
-import { InputGroup, FormControl, Form, Button, Modal, Collapse } from 'react-bootstrap';
+import {
+  InputGroup,
+  FormControl,
+  Form,
+  Button,
+  Modal,
+  Collapse,
+} from 'react-bootstrap';
 import '../AddFridgeItem/AddFridgeItem.module.scss';
 
 const now = new Date();
@@ -16,35 +23,55 @@ export default class extends Component {
     year: String(now.getFullYear()),
     month: String(now.getMonth() + 1),
     day: String(now.getDate()),
-  }
+  };
   //Refact - перенести запрос в reduxThunk
-  setOpen = () => { this.setState({ open: !this.state.open }) };
+  setOpen = () => {
+    this.setState({ open: !this.state.open });
+  };
 
-  setOpenCalendar = () => { this.setState({ isSelectDateOpen: !this.state.isSelectDateOpen }) };
+  setOpenCalendar = () => {
+    this.setState({ isSelectDateOpen: !this.state.isSelectDateOpen });
+  };
 
   addItem = async () => {
-    const timeDiff = Math.abs(new Date(this.state.year, this.state.month - 1, this.state.day) - now);
+    const timeDiff = Math.abs(
+      new Date(this.state.year, this.state.month - 1, this.state.day) - now
+    );
     const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    await this.setState({dayRemaining: diffDays, expiryDate: new Date(this.state.year, this.state.month - 1, this.state.day)});
+    await this.setState({
+      dayRemaining: diffDays,
+      expiryDate: new Date(
+        this.state.year,
+        this.state.month - 1,
+        this.state.day
+      ),
+    });
     const item = {
       label: this.state.label,
       expiryDate: this.state.expiryDate,
       category: this.state.category,
       dayRemaining: this.state.dayRemaining,
-    }
+    };
     await fetch('/fridge/add', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json;charset=utf-8'
+        'Content-Type': 'application/json;charset=utf-8',
       },
       body: JSON.stringify(item),
-    })
-    this.setState({ open: !this.state.open, label: '', expiryDate: now, category: 'Other', dayRemaining: 0, isSelectDateOpen: false });
-  }
+    });
+    this.setState({
+      open: !this.state.open,
+      label: '',
+      expiryDate: now,
+      category: 'Other',
+      dayRemaining: 0,
+      isSelectDateOpen: false,
+    });
+  };
 
   onChangeValue = event => {
-    this.setState({ label: event.target.value })
-  }
+    this.setState({ label: event.target.value });
+  };
 
   changeExpireYear = event => {
     this.setState({ year: event.target.value });
@@ -58,7 +85,6 @@ export default class extends Component {
     this.setState({ day: event.target.value });
   };
 
-
   render() {
     return (
       <>
@@ -69,13 +95,18 @@ export default class extends Component {
           </Modal.Header>
           <Modal.Body>
             Item
-            <InputGroup size="lg">
-              <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" onChange={this.onChangeValue} value={this.state.label} />
+            <InputGroup size='lg'>
+              <FormControl
+                aria-label='Large'
+                aria-describedby='inputGroup-sizing-sm'
+                onChange={this.onChangeValue}
+                value={this.state.label}
+              />
             </InputGroup>
             <Form>
-              <Form.Group controlId="exampleForm.SelectCustom">
+              <Form.Group controlId='exampleForm.SelectCustom'>
                 <Form.Label>Category</Form.Label>
-                <Form.Control as="select" custom>
+                <Form.Control as='select' custom>
                   <option>Other</option>
                   <option>Vegetables</option>
                   <option>Fruit</option>
@@ -86,21 +117,37 @@ export default class extends Component {
             </Form>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant='outline-primary' onClick={this.setOpenCalendar} aria-controls="example-collapse-text" aria-expanded={this.state.isSelectDateOpen} type='submit' value='expireDate'>Expiry Date: {this.state.year}.{this.state.month}.{this.state.day}</Button>
-            <Button variant="primary" onClick={this.addItem} type="submit" value='addItem'>Add item</Button>
+            <Button
+              variant='outline-primary'
+              onClick={this.setOpenCalendar}
+              aria-controls='example-collapse-text'
+              aria-expanded={this.state.isSelectDateOpen}
+              type='submit'
+              value='expireDate'
+            >
+              Expiry Date: {this.state.year}.{this.state.month}.{this.state.day}
+            </Button>
+            <Button
+              variant='primary'
+              onClick={this.addItem}
+              type='submit'
+              value='addItem'
+            >
+              Add item
+            </Button>
             <Collapse in={this.state.isSelectDateOpen}>
-              <div id="example-collapse-text">
-              <SelectDate
-                state={String(this.state)}
-                changeExpireYear={this.changeExpireYear}
-                changeExpireMonth={this.changeExpireMonth}
-                changeExpireDay={this.changeExpireDay}
-              />
+              <div id='example-collapse-text'>
+                <SelectDate
+                  state={String(this.state)}
+                  changeExpireYear={this.changeExpireYear}
+                  changeExpireMonth={this.changeExpireMonth}
+                  changeExpireDay={this.changeExpireDay}
+                />
               </div>
             </Collapse>
           </Modal.Footer>
         </Modal>
       </>
-    )
+    );
   }
 }
