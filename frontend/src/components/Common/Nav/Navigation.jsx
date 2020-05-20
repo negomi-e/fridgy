bed477a60a8ecbfd2507190b3b48456656ef8f5a
Imogen <imogen.eddings@gmail.com>
2020-05-20 17:09:18 +0100

    remove search bar

--- frontend/src/components/Common/Nav/Navigation.jsx
+++ frontend/src/components/Common/Nav/Navigation.jsx
@@ -41,12 +41,6 @@ class Navigation extends Component {
           </Navbar.Collapse>
         </Container>
 
-       
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
 
       </Navbar>
     )
