fe10b1954e0d45fa7c2d8e86ca7cf49976616198
PAKD <setevieru@yandex.ru>
2020-05-20 09:18:06 +0300

    recipe page css update

--- frontend/src/components/Pages/Recipe/RenderRecipePage.jsx
+++ frontend/src/components/Pages/Recipe/RenderRecipePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { loadItems, takeItems } from '../../../redux/Actions/fridge-actions'
+import "./recipe.css";
+import {Button} from 'reactstrap'
 
 class RenderRecipePage extends React.Component {
 
@@ -9,19 +11,27 @@ class RenderRecipePage extends React.Component {
 
     console.log(this.props)
     return (
-      <div>
-        <h3></h3>
-        <h3>{this.props.itemsApi &&
+      <div className='wrapped-recipe-card'>
+        {this.props.itemsApi &&
           this.props.itemsApi[0].map((item) => {
-            return <><div><h1>{item.display.displayName}</h1></div><br></br>
-              <img style={{ borderRadius: 20 }} src={item.display.images[0]}></img>
-              <div style={{color: "black"}}>Ingredients: {item.content.ingredientLines.map((item) => { return <li>{item.wholeLine}</li> })}</div><br></br>
-              <div style={{ color: "black" }}>Ingridients: {item.content.ingredientLines.length} items</div>
-              <div style={{ color: "black" }}>Total Time: {item.content.details.totalTime} </div>
-              <div style={{ color: "black" }}>Total Fat Calories: {item.content.nutrition.nutritionEstimates[0].display.value}Cal</div>
-              <div><a target='_blank' rel='noopener noreferrer' href={item.content.details.directionsUrl}><h2>Show Recipe!</h2></a></div>
+            return <>
+              <div className="display-container">
+              <div className="display-named-items-container">
+              <div className='display-named'>{item.display.displayName}</div>
+              <div className="display-items">{item.content.ingredientLines.length} 
+              <div className="display-ingredient">items</div></div>
+              <div className="display-items">{item.content.details.totalTime} 
+              <div className="display-ingredient">minutes</div></div>
+              <div className="display-items">{item.content.nutrition.nutritionEstimates[0].display.value}
+              <div className="display-ingredient">Calories</div>
+              </div></div>
+              <div className='display-image'><img className="img" style={{ borderRadius: 10 }} src={item.display.images[0]}></img></div></div>
+              <div className="display-ingredient" ><div className="ingredients">Ingredients:</div> {item.content.ingredientLines.map((item) => { return <li>{item.wholeLine}</li> })}</div><br></br>
+              <div><Button color="warning"><a className="display-a" target='_blank' rel='noopener noreferrer' href={item.content.details.directionsUrl}>Show Recipe!</a></Button></div>
+              
+             
             </>
-          })}</h3>
+          })}
 
       </div>
     )
