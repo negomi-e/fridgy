2a2da25a3b358f16d6f6eb6abc9b1376fe1de4a1
Imogen <imogen.eddings@gmail.com>
2020-05-22 11:42:48 +0100

    resolved conflicts

--- frontend/src/components/Pages/Home/OurApp.jsx
+++ frontend/src/components/Pages/Home/OurApp.jsx
@@ -15,11 +15,7 @@ class OurApp extends Component {
 
         <div id="wrapper">
           <div className="phone view_1" id="phone_1">
-<<<<<<< HEAD
-            <iframe src="http://designhooks.com" id="frame_123" key="frame_1"></iframe>
-=======
             <iframe src="http://fridgy-2020.herokuapp.com/" id="frame_1"></iframe>
->>>>>>> 437cf759965568371ca06728a6893827155d7663
           </div>
         </div>
       </div>
